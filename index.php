dfb73b9cd7e2ea0d9a7138840f066ffd76dc6423
Pilar <Pilar@LAPTOP-0CAFFMA9.mshome.net>
2021-01-24 19:44:23 +0100

    Primer commit de mi primer ejercicio Git

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Este es mi primer ejercicio para subir a Git";
+        echo "Esta es la primera modificación a mi proyecto";
+        ?>
+    </body>
+</html>
